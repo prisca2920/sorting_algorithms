d56cba8bd2d77b8ef762bae68ed980c48060a765
prisca2920 <prisca2920@gmail.com>
2023-11-19 23:53:20 -0800

    bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -11,9 +11,9 @@ void bubble_sort(int *array, size_t size)
 {
 	size_t i, j, temp;
 
-	for (i = 0; i < size - 1; i++)
+	for (i = 0; i < size; i++)
 	{
-	for (j = 0; j < size - 1 - i; j++)
+	for (j = 0; j < size - 1; j++)
 	{
 	if (array[j] > array[j + 1])
 	{

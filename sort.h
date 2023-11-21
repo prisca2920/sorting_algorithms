64964e4fcf7476815102572a54283f269e66e17a
prisca2920 <prisca2920@gmail.com>
2023-11-20 23:31:36 -0800

    header file

--- sort.h
+++ sort.h
@@ -23,6 +23,6 @@ void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
-void _swap(listint_t **node, listint_t **list);
+void _swaps(listint_t **node, listint_t **list);
 
 #endif
